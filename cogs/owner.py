963d128c566a96fcbde9069616ea3b1b84ff18ec
Shreyash Raj <shreyashr267@gmail.com>
2022-07-03 14:14:08 +0530

    Updated cog command + Removed sqlite and added json to control cogs

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,25 +14,14 @@ SQLITE_FILE_NAME="json.sqlite"
 class OwnerOnly(commands.Cog):
   def __init__(self, bot: Shashank):
     self.bot: Shashank = bot
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    con.execute(f'CREATE TABLE IF NOT EXISTS COGS (ID text, path text)')
-    cogs = con.execute(f"SELECT * FROM COGS").fetchall() or []
-    con.close()
-    for cog_dict in cogs:
+    with open('cogs/cogs.json') as f:
+      self.cogs = json.load(f)
+    
+    for key in self.cogs.keys():
       try:
-        bot.load_extension(cog_dict[1])
+        bot.load_extension(self.cogs[key])
       except ExtensionAlreadyLoaded:
         pass
-
-  def _get_(self, name):
-    """
-    Get data from a cog name.
-    returns (ID, path)
-    """
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    data = con.execute(f'SELECT * FROM COGS WHERE ID="{name}"').fetchone() or None
-    con.close()
-    return data
   
   @commands.command(name='eval', hidden=True)
   @commands.is_owner()
@@ -90,74 +79,34 @@ class OwnerOnly(commands.Cog):
 
   async def cog_check(self, ctx):
     return await self.bot.is_owner(ctx.author)
-
-  def _get_all_(self):
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    data = con.execute(f'SELECT * FROM COGS').fetchall() or []
-    con.close()
-    return data
-  
-  def _add_(self, cog_name: str, cog_path: str):
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    if self._has_(cog_name):
-      con.close()
-      return "Already Added."
-    con.execute(f"INSERT INTO COGS VALUES ('{cog_name}', '{cog_path}')")
-    con.commit()
-    con.close()
-    return True
-  
-  def _has_(self, name: str):
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    cog = con.execute(f'SELECT * FROM COGS WHERE ID="{name}"').fetchall()
-    con.close()
-    return True if len(cog) > 0 else False
-
-  def _remove_(self, name: str):
-    con = sqlite3.connect(SQLITE_FILE_NAME)
-    con.execute(f"DELETE from COGS WHERE ID='{name}'")
-    con.commit()
-    con.close()
-    return True
   
   @commands.is_owner()
   @commands.command()
-  async def load(self, ctx: commands.Context, name: str, path: str):
-    self.bot.load_extension(path)
-    embed=disnake.Embed(color=disnake.Color.blurple(), title="Load Extension", description=f"Loaded extension **{name}** from `{path}`.")
-    self._add_(name, path)
+  async def load(self, ctx: commands.Context, name: str):
+    self.bot.load_extension(self.cogs[name])
+    embed=disnake.Embed(color=disnake.Color.blurple(), title="Load Extension", description=f"Loaded extension **{name}** from `{self.cogs[name]}`.")
     return await ctx.reply(embed=embed)
 
   @commands.is_owner()
   @commands.command()
   async def unload(self, ctx: commands.Context, name: str):
-    data = self._get_(name=name)
+    data = self.cogs[name]
     if data is None:
       raise disnake.NotFound(f"Module {name} is not found in database.")
     
-    self.bot.unload_extension(data[1])
-    embed=disnake.Embed(color=disnake.Color.blurple(), title="Unload Extension", description=f"Unloaded extension **{name}** from `{data[1]}`.")
-    self._remove_(name)
+    self.bot.unload_extension(data)
+    embed=disnake.Embed(color=disnake.Color.blurple(), title="Unload Extension", description=f"Unloaded extension **{name}** from `{data}`.")
     return await ctx.reply(embed=embed)
     
   @commands.is_owner()
   @commands.command(name='reload')
   async def _reload(self, ctx: commands.Context, name: str):
-      data=self._get_(name=name)
-      if data is not None:
-        self.bot.reload_extension(data[1])
-        embed=disnake.Embed(color=disnake.Color.blurple(), title="Reload Extension", description=f"Reloaded extension **{name}** from `{data[1]}`.")
-        # self._add_(name, data[name])
-        return await ctx.reply(embed=embed)
-      else:
-        return await ctx.reply(f"Module {name} is not loaded.")
+      pass
 
   @commands.is_owner()
   @commands.command()
   async def block(self, ctx: commands.Context, user: disnake.User):
-    self.bot.db.push('blocked_users', user.id)
-    embed=disnake.Embed(title="User Blocked", description=f"**{user.name}#{user.discriminator}** is blocked from using the bot.", color=disnake.Color.blurple())
-    return await ctx.reply(embed=embed)
+    pass
 
   @commands.is_owner()
   @commands.command()
